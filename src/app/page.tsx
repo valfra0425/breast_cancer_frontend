9b0e16bedf4a9323735c98174a5e34a461ec2cc4
valfra0425 <valmirfrancisco1@outlook.com>
2024-05-14 17:40:01 -0300

    tela inicial finalizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,11 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={`${styles.center}`}>
         <div className={`${styles.divButton} ${styles.center}`}>
           <button type="button" className={`${styles.btn}`}>Começar a Detecção</button>
-          <p className={styles.white}>Nosso modelo de dectecção foi feito utilizando os dados da competição "RSNA Screening Mammography Breast Cancer Detection AI Challenge (2023)".</p>
-          <p className={styles.white}> Acesse o link: <a className={styles.linkStyle} href="https://www.kaggle.com/competitions/rsna-breast-cancer-detection/overview">RSNA Screening Mammography Breast Cancer Detection</a> para saber mais sobre os dados utilizados.</p>
+          <p className={styles.text}>Nosso modelo de dectecção foi feito utilizando os dados da competição "RSNA Screening Mammography Breast Cancer Detection AI Challenge (2023)".</p>
+          <p className={styles.text}> Acesse o link: <a className={styles.linkStyle} href="https://www.kaggle.com/competitions/rsna-breast-cancer-detection/overview">RSNA Screening Mammography Breast Cancer Detection</a> para saber mais sobre os dados utilizados.</p>
         </div>
-      </div>
-      <div className={`${styles.divLogo} ${styles.center}`}>
-        <Image src="/ribbon.png" alt="Fita da campanha contra o cancer de mama" width={300} height={300}/>
-        <p className={styles.textLogo}>Prevenir é o melhor caminho</p>
-      </div>
     </main>
   );
 }
