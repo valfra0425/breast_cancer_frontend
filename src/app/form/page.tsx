7f6762a7868689a76e242704481fdd78999388ab
valfra0425 <valmirfrancisco1@outlook.com>
2024-05-16 18:08:18 -0300

    add modal

---
+++ src/app/form/page.tsx
@@ -0,0 +1,5 @@
+export default function Form(){
+    return (
+        <p>Olá mundo</p>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
-import Image from "next/image";
+'use client'
+
+import { useState } from "react";
+import "./globals.css";
 import styles from "./page.module.css";
+import Modal from "@/app/components/modal/modal"
 
 export default function Home() {
+  const [modalIsOpen, setModalIsOPen] = useState(false)
+
+  function handleModal(){
+    setModalIsOPen(!modalIsOpen);
+  }
+
   return (
     <main className={styles.main}>
         <div className={`${styles.divButton} ${styles.center}`}>
-          <button type="button" className={`${styles.btn}`}>Começar a Detecção</button>
+          <button type="button" className={`button ${styles.button}`} onClick={handleModal}>Começar a Detecção</button>
           <p className={styles.text}>Nosso modelo de dectecção foi feito utilizando os dados da competição "RSNA Screening Mammography Breast Cancer Detection AI Challenge (2023)".</p>
-          <p className={styles.text}> Acesse o link: <a className={styles.linkStyle} href="https://www.kaggle.com/competitions/rsna-breast-cancer-detection/overview">RSNA Screening Mammography Breast Cancer Detection</a> para saber mais sobre os dados utilizados.</p>
+          <p className={styles.text}> Acesse o link: <a href="https://www.kaggle.com/competitions/rsna-breast-cancer-detection/overview">RSNA Screening Mammography Breast Cancer Detection</a> para saber mais sobre os dados utilizados.</p>
         </div>
+        <Modal isOpen={modalIsOpen} isClosed={handleModal}></Modal>
     </main>
   );
 }
